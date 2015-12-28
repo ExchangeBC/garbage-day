e92f34442325775dd9ccdab17c71d71f9711b451
Jonathan Donas <jcontact97@gmail.com>
2015-12-28 23:39:17 +0000

    further cleaned up code. Returns zone number

--- code/api/app.py
+++ code/api/app.py
@@ -1,15 +1,11 @@
-# Get ready to see some bollocks
-# thanks
-
 import requests
 from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route('/getzone')
-def json_blob(): # < here
+def json_blob():
     address = request.args['address']
-    print(address)
     govturl = "http://maps.kamloops.ca/arcgis3/rest/services/BCDevExchange/GarbagePickup/MapServer/3/query"
     payload = {"geometryType":"esriGeometryEnvelope",
         "where":"ADDRESS = '" + address + "'",
@@ -24,10 +20,10 @@ def json_blob(): # < here
         "f":"pjson",
         "returnDistinctValues":"false"}
     r = requests.get(govturl, params=payload)
-    print r.url
     jsonblob = r.text
-    return jsonblob
-
+    jdict = json.loads(jsonblob)
+    zone = jdict["features"][0]["attributes"]["ZONE"]
+    return zone
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
 
